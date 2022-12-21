e99453067dc31d45ce4566361a1d752fca8c8745
hyunjung <hhhj.1486@gmail.com>
2022-12-21 19:30:38 +0900

    feature: 키워드 분석
    ETRI 형태소 분석 API 적용하여 키워드 분석 기능 구현 (미완)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const keywordController = require("../controller/keywordController");
+require("dotenv").config();
 
 const router = express.Router();
+
+router.post("/keyword", keywordController.keywordAnalyze);
 
 module.exports = router;
