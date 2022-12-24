e615aa419210f87cde5b46ad234273f0dd79a656
전준영 <wnsdud9476@DESKTOP-2VICEL1>
2022-12-24 16:07:31 +0900

    Modify : regexp

--- src/services/wishServices.js
+++ src/services/wishServices.js
@@ -1,5 +1,5 @@
 const wishDao = require("../models/wishDao");
-const { nickNameRegex, commentFilter } = require("../common/regex");
+const { nickNameRegex, badWordRegex, commentLengthRegex } = require("../common/regex");
 const { ValidationError } = require("../middleware/errorCreator");
 const { getCurrentDate } = require("../common/date");
 
@@ -8,22 +8,31 @@ const wishCreateService = async (uuid, nickName, comment) => {
 
   if (
     uuidCheck.length &&
-    uuidCheck[0].createdAt.toLocaleDateString("ko-KR", { timeZone: "UTC" }) ===
+    uuidCheck[uuidCheck.length - 1].createdAt.toLocaleDateString("ko-KR", { timeZone: "UTC" }) ===
       getCurrentDate().toLocaleDateString("ko-KR", { timeZone: "UTC" })
   ) {
     throw new ValidationError("오늘 이미 소원을 작성한 유저입니다.");
   }
 
-  const regex = nickNameRegex;
-  const regexTest = regex.test(nickName);
-  const badWord = commentFilter;
-  const badWordTest = badWord.test(comment);
+  const nickNameFilter = nickNameRegex;
+  const regexTest = nickNameFilter.test(nickName);
+  const badWordFilter = badWordRegex;
+  const nickNameBadWordTest = badWordFilter.test(nickName);
+  const badWordTest = badWordFilter.test(comment);
+  const commentLengthFilter = commentLengthRegex;
+  const commentLenthTest = commentLengthFilter.test(comment);
 
   if (!regexTest) {
     throw new ValidationError("특수문자 제외 한글 또는 영문 숫자를 포함한 8글자 이내여야 합니다.");
   }
+  if (nickNameBadWordTest) {
+    throw new ValidationError("비속어는 사용 금지입니다.");
+  }
   if (badWordTest) {
     throw new ValidationError("비속어는 사용 금지입니다.");
+  }
+  if (!commentLenthTest) {
+    throw new ValidationError("200글자 이하로 작성해주십시오");
   }
   return wishDao.createWish(uuid, nickName, comment);
 };
