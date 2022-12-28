7ddb9199d9affc6f8cc491eef8bd80789caa6264
hyunjung <hhhj.1486@gmail.com>
2022-12-28 16:14:58 +0900

    Modify: 키워드 분석 기능 수정
    특정 시간의 소원들을 뽑아와서 총 개수를 구한 다음, 50건 단위로 키워드 분석을 할 수 있도록 기능 구현(미완)

--- src/controller/keywordController.js
+++ src/controller/keywordController.js
@@ -1,37 +1,9 @@
-var request = require("request");
+const request = require("request");
+const keywordService = require("../service/keywordService");
 require("dotenv").config();
 
-const keywordAnalyze = (req, res) => {
-  let text = "복권 당첨시켜주세요";
-  // DB에서 데이터 읽어와서 text에 값 넘겨주어야 함. 1만글자까지 가능
-
-  var requestJson = {
-    argument: {
-      text: text,
-      analysis_code: process.env.ANALYSISCODE,
-    },
-  };
-
-  var options = {
-    url: process.env.API_URI,
-    body: JSON.stringify(requestJson),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: process.env.API_KEY,
-    },
-  };
-
-  request.post(options, function (error, response, body) {
-    let bodyJsonParse = JSON.parse(body);
-    let morp = bodyJsonParse.return_object.sentence[0].morp;
-    let nounArr = [];
-
-    morp.forEach((e) => {
-      if (e.type === "NNG") {
-        console.log(e.lemma);
-      }
-    });
-  });
+const keywordAnalyze = async (req, res) => {
+  await keywordService.keywordAnalyze();
 };
 
 module.exports = { keywordAnalyze };
