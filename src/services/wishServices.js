2bdc3ac89cf622c616929d3f117af6a7ed1a1a55
hhhj1008 <110225060+hhhj1008@users.noreply.github.com>
2023-01-16 13:10:05 +0900

    fix: 키워드검색/내가작성한 소원 목록 오류 수정
    좋아요 여부를 체크할 때 배열 전체가 아닌 하나의 건에 대해서만 체크를 하여 보냈기 때문에 배열이 아닌 객체로 가던 오류 수정

--- src/services/wishServices.js
+++ src/services/wishServices.js
@@ -1,29 +1,20 @@
-const wishDao = require("../models/wishDao");
-const {
-  nickNameRegex,
-  badWordRegex,
-  commentLengthRegex,
-} = require("../common/regex");
-const {
-  ValidationError,
-  BadRequestError,
-} = require("../middleware/errorCreator");
-const { getCurrentDate } = require("../common/date");
+const wishDao = require('../models/wishDao');
+const { nickNameRegex, badWordRegex, commentLengthRegex } = require('../common/regex');
+const { ValidationError, BadRequestError } = require('../middleware/errorCreator');
+const { getCurrentDate } = require('../common/date');
 
-const wishDuplicationCheck = async (uuid) => {
+const wishDuplicationCheck = async uuid => {
   const uuidCheck = await wishDao.findWishByUuid(uuid);
   if (!uuidCheck.length) return;
-  const latestWishTime = uuidCheck[
-    uuidCheck.length - 1
-  ].createdAt.toLocaleDateString("ko-KR", {
-    timeZone: "UTC",
+  const latestWishTime = uuidCheck[uuidCheck.length - 1].createdAt.toLocaleDateString('ko-KR', {
+    timeZone: 'UTC',
   });
-  const currentTime = getCurrentDate().toLocaleDateString("ko-KR", {
-    timeZone: "UTC",
+  const currentTime = getCurrentDate().toLocaleDateString('ko-KR', {
+    timeZone: 'UTC',
   });
 
   if (uuidCheck.length && latestWishTime === currentTime) {
-    throw new ValidationError("Already created");
+    throw new ValidationError('Already created');
   }
 };
 
@@ -37,18 +28,16 @@ const wishCreateService = async (uuid, nickName, comment) => {
   const commentLenthTest = commentLengthFilter.test(comment);
 
   if (!regexTest) {
-    throw new ValidationError(
-      "특수문자 제외 한글 또는 영문 숫자를 포함한 8글자 이내여야 합니다."
-    );
+    throw new ValidationError('특수문자 제외 한글 또는 영문 숫자를 포함한 8글자 이내여야 합니다.');
   }
   if (nickNameBadWordTest) {
-    throw new ValidationError("비속어는 사용 금지입니다.");
+    throw new ValidationError('비속어는 사용 금지입니다.');
   }
   if (badWordTest) {
-    throw new ValidationError("비속어는 사용 금지입니다.");
+    throw new ValidationError('비속어는 사용 금지입니다.');
   }
   if (!commentLenthTest) {
-    throw new ValidationError("200글자 이하로 작성해주십시오");
+    throw new ValidationError('200글자 이하로 작성해주십시오');
   }
   return wishDao.createWish(uuid, nickName, comment);
 };
@@ -69,34 +58,38 @@ const detailWishForMainService = async (id, uuid) => {
 
 const findWishByKeyword = async (keyword, skip, limit, uuid) => {
   if (keyword.length == 0) {
-    throw new BadRequestError("키워드에는 null 값이 들어갈 수 없습니다.");
+    throw new BadRequestError('키워드에는 null 값이 들어갈 수 없습니다.');
   }
 
-  const regex = (pattern) => new RegExp(`.*${pattern}.*`);
+  const regex = pattern => new RegExp(`.*${pattern}.*`);
   const keywordRegex = regex(keyword);
   skip = (skip - 1) * limit;
 
-  const [searchWishes] = await wishDao.findWishByKeyword(
-    keywordRegex,
-    skip,
-    limit
-  );
-  if (searchWishes.likeUser.includes(uuid)) {
-    searchWishes.isLike = true;
-  } else {
-    searchWishes.isLike = false;
+  const searchWishes = await wishDao.findWishByKeyword(keywordRegex, skip, limit);
+
+  for (let i = 0; i < searchWishes.length; i++) {
+    if (searchWishes[i].likeUser.includes(uuid)) {
+      searchWishes[i].isLike = true;
+    } else {
+      searchWishes[i].isLike = false;
+    }
   }
+
   return searchWishes;
 };
 
 const findMyWishList = async (uuid, skip, limit) => {
   skip = (skip - 1) * limit;
-  const [myWishes] = await wishDao.findMyWishList(uuid, skip, limit);
-  if (myWishes.likeUser.includes(uuid)) {
-    myWishes.isLike = true;
-  } else {
-    myWishes.isLike = false;
+  const myWishes = await wishDao.findMyWishList(uuid, skip, limit);
+
+  for (let i = 0; i < myWishes.length; i++) {
+    if (myWishes[i].likeUser.includes(uuid)) {
+      myWishes[i].isLike = true;
+    } else {
+      myWishes[i].isLike = false;
+    }
   }
+
   return myWishes;
 };
 
