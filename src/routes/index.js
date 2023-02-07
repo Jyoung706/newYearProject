1d15cf0270e23a612eee123e09d36542e9ec7741
hyunjung <hhhj.1486@gmail.com>
2023-02-07 16:11:30 +0900

    Modify: 키워드 분석 예외처리
    키워드 분석 시 수,곳,러 등 이상하게 분석되는 단어는 분석되지 않도록 예외처리 기능 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const asyncWrap = require("../middleware/asyncWrap");
-const wishController = require("../controllers/wishController");
-const idController = require("../controllers/idController");
-const likeController = require("../controllers/likeController");
-const keywordController = require("../controllers/keywordController");
+const express = require('express');
+const asyncWrap = require('../middleware/asyncWrap');
+const wishController = require('../controllers/wishController');
+const idController = require('../controllers/idController');
+const likeController = require('../controllers/likeController');
+const keywordController = require('../controllers/keywordController');
 
 const router = express.Router();
 
-router.get("/keyword", asyncWrap(keywordController.findUsuallyKeyword));
-router.get("/id", idController.idCreateController);
-router.post("/wishes", asyncWrap(wishController.wishCreateController));
-router.post("/like", asyncWrap(likeController.likeController));
-router.get("/main", wishController.wishForMainController);
-router.get("/like", likeController.myLikeController);
-router.get("/wishes", asyncWrap(wishController.detailWishForMainController));
-router.get("/search", asyncWrap(wishController.findWishByKeyword));
-router.get("/mywish", asyncWrap(wishController.findMyWishList));
-router.get("/wish-count", wishController.wishCountController);
-router.get("/wish-check", asyncWrap(wishController.wishDuplicationCheck));
+router.get('/keyword', asyncWrap(keywordController.findUsuallyKeyword));
+router.get('/id', idController.idCreateController);
+router.post('/wishes', asyncWrap(wishController.wishCreateController));
+router.post('/like', asyncWrap(likeController.likeController));
+router.get('/main', wishController.wishForMainController);
+router.get('/like', likeController.myLikeController);
+router.get('/wishes', asyncWrap(wishController.detailWishForMainController));
+router.get('/search', asyncWrap(wishController.findWishByKeyword));
+router.get('/mywish', asyncWrap(wishController.findMyWishList));
+router.get('/wish-count', wishController.wishCountController);
+router.get('/wish-check', asyncWrap(wishController.wishDuplicationCheck));
+router.post('/analyze', asyncWrap(keywordController.keywordAnalyze));
 
 module.exports = router;
--- src/services/keywordService.js
+++ src/services/keywordService.js
@@ -1,34 +1,42 @@
-const request = require("request");
-require("dotenv").config();
-const wishDao = require("../models/wishDao");
-const keyWordDao = require("../models/keyWordDao");
-const { getCurrentTime, getPreviousTime } = require("../common/date");
+const request = require('request');
+require('dotenv').config();
+const wishDao = require('../models/wishDao');
+const keyWordDao = require('../models/keyWordDao');
+const { getCurrentTime, getPreviousTime } = require('../common/date');
+const { exceptKeyword } = require('../common/exceptKeyword');
 
-const keywordAnalyze = async () => {
+const keywordAnalyze = async type => {
   let pre_time = getPreviousTime();
   let curr_time = getCurrentTime();
 
-  const count = await wishDao.findWishListByTimeCount(pre_time, curr_time);
+  let count = 0;
+  let wishList = null;
+
+  if (type === 'all') {
+    count = await wishDao.findWishListCount();
+  } else {
+    count = await wishDao.findWishListByTimeCount(pre_time, curr_time);
+  }
+
   let wishDiv = Math.ceil(count / 50);
 
   let limit = 50;
 
   for (let i = 1, pending = Promise.resolve(); i <= wishDiv; i++) {
-    let text = "";
+    let text = '';
 
-    let wishList = await wishDao.findWishListByTime(
-      pre_time,
-      curr_time,
-      i * limit - limit,
-      limit
-    );
+    if (type === 'all') {
+      wishList = await wishDao.findAllWishList();
+    } else {
+      wishList = await wishDao.findWishListByTime(pre_time, curr_time, i * limit - limit, limit);
+    }
 
     for (let x = 0; x < wishList.length; x++) {
-      text = text + wishList[x].comment;
+      text = text + wishList[x].comment + ' ';
     }
 
     pending = pending.then(() => {
-      return new Promise((resolve) => {
+      return new Promise(resolve => {
         setTimeout(function () {
           resolve(callMorphemeAPI(text));
         }, 1000);
@@ -37,7 +45,7 @@ const keywordAnalyze = async () => {
   }
 };
 
-const callMorphemeAPI = (text) => {
+const callMorphemeAPI = text => {
   let requestJson = {
     argument: {
       text: text,
@@ -49,7 +57,7 @@ const callMorphemeAPI = (text) => {
     url: process.env.API_URI,
     body: JSON.stringify(requestJson),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       Authorization: process.env.API_KEY,
     },
   };
@@ -60,13 +68,15 @@ const callMorphemeAPI = (text) => {
     let bodyJsonParse = JSON.parse(body);
     let morp = bodyJsonParse.return_object.sentence[0].morp;
 
-    morp.forEach((e) => {
-      if (e.type === "NNG" || e.type === "NNP" || e.type === "NNB") {
-        if (map.get(e.lemma)) {
-          let num = map.get(e.lemma);
-          map.set(e.lemma, num + 1);
-        } else {
-          map.set(e.lemma, 1);
+    morp.forEach(e => {
+      if (e.type === 'NNG' || e.type === 'NNP' || e.type === 'NNB') {
+        if (!exceptKeyword.test(e.lemma)) {
+          if (map.get(e.lemma)) {
+            let num = map.get(e.lemma);
+            map.set(e.lemma, num + 1);
+          } else {
+            map.set(e.lemma, 1);
+          }
         }
       }
     });
